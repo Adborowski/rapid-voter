efeac6d29db5c1de17c5b9680ce5bd640fcff593
Adam Borowski <adborowski@gmail.com>
2023-07-11 17:11:54 +0200

    added link component to roomlink and handled redirection

--- src/components/room-link/room-link.tsx
+++ src/components/room-link/room-link.tsx
@@ -1,4 +1,5 @@
 import styles from "./room-link.module.css";
+import Link from "next/link";
 
 type RoomLinkProps = {
     roomId: string;
@@ -6,7 +7,11 @@ type RoomLinkProps = {
 
 const RoomLink = (props: RoomLinkProps) => {
     const { roomId } = props;
-    return <div className={styles.RoomLink}>roomlink to {roomId}</div>;
+    return (
+        <div className={styles.RoomLink}>
+            <Link href={"/room/" + roomId}>Go to room</Link>
+        </div>
+    );
 };
 
 export default RoomLink;

8280c37dac5af3df7d5bc5c392260e1065b33ee2
Adam Borowski <adborowski@gmail.com>
2023-08-06 13:59:39 +0200

    removed bugged import from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import Header from "@/components/header/header";
-import RoomEditor from "@/components/room-editor/room-editor";
-import { connectDatabase } from "../utils/db-util";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import Header from '@/components/header/header'
+import RoomEditor from '@/components/room-editor/room-editor'
 
 export default function Home(props: any) {
-    return <RoomEditor />;
+   return <RoomEditor />
 }

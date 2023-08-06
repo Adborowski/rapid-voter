44a78bb3ce27235144bb2c02f5c3c077e73318c9
Adam Borowski <adborowski@gmail.com>
2023-08-06 14:00:03 +0200

    removed unused imports from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import Header from '@/components/header/header'
 import RoomEditor from '@/components/room-editor/room-editor'
 
 export default function Home(props: any) {

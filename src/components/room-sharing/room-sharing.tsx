f5578af4900d5af1313bb1183eb93ca52da2b1be
Adam Borowski <adborowski@gmail.com>
2023-10-02 21:33:03 +0200

    push again to rebuild

---
+++ src/components/room-sharing/room-sharing.tsx
@@ -0,0 +1,12 @@
+import QRCode from 'react-qr-code'
+import styles from './room-sharing.module.css'
+
+const RoomSharing = (props: any) => {
+   return (
+      <div className={styles.roomSharing}>
+         <QRCode value={location.href} />
+      </div>
+   )
+}
+
+export default RoomSharing
